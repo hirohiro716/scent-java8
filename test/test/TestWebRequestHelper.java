ab1402a980809e3f622dfbaf998237d5bcae5a64
hiro <hiro@nova>
2019-04-09 13:59:19 +0900

    none

--- test/test/TestWebRequestHelper.java
+++ test/test/TestWebRequestHelper.java
@@ -9,11 +9,15 @@ public class TestWebRequestHelper {
 
     public static void main(String[] args) {
         WEBRequest helper = new WEBRequest("https://www.aketa.jp/aketadmin/");
-        helper.putParam("login_id", "admin");
-        helper.putParam("password", "password");
+        
+        
+        
+        helper.putParam("login_id", "hiro");
+        helper.putParam("password", "Hiro6295");
         try {
             System.out.println(helper.requestPostResult());
         } catch (IOException exception) {
+            exception.printStackTrace();
         }
     }
 
