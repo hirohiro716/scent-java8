17b41090a2264bdb8e5bdd95f0c88ae1ae451a20
hiro <hiro@nova>
2022-05-31 17:24:13 +0900

    Fixed bug

--- src/com/hirohiro716/ExceptionHelper.java
+++ src/com/hirohiro716/ExceptionHelper.java
@@ -26,11 +26,23 @@ public class ExceptionHelper {
     public static String createDetailMessage(String newMessage, Exception exception) {
         exception.printStackTrace();
         StringBuilder message = new StringBuilder();
-        if (newMessage != null && newMessage.length() > 0) {
-            message.append(newMessage);
+        if (message.length() > 0) {
+            message.append(LINE_SEPARATOR);
             message.append(LINE_SEPARATOR);
         }
-        message.append(exception.getClass().getName());
+        message.append("An exception occurred in ");
+        StackTraceElement stackTraceElement = exception.getStackTrace()[0];
+        message.append(stackTraceElement.getClassName());
+        message.append(":");
+        message.append(stackTraceElement.getLineNumber());
+        message.append(LINE_SEPARATOR);
+        if (exception.getMessage() != null && exception.getMessage().length() > 0) {
+            message.append(exception.getClass().getName());
+            message.append(": ");
+            message.append(exception.getMessage());
+        } else {
+            message.append(exception.getClass().getName());
+        }
         return message.toString();
     }
     
